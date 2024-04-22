b3b213f6b3f51c650115e58663dc3cedee504f72
crudapi <admin@crudapi.cn>
2024-04-22 17:27:18 +0800

    cout

--- src/CMakeProject/CMakeProject.cpp
+++ src/CMakeProject/CMakeProject.cpp
@@ -8,5 +8,18 @@ using namespace std;
 int main()
 {
 	cout << "Hello CMake." << endl;
+
+    int myAge = 18;
+    cout << myAge << endl;
+
+    double pi = 3.14159;
+    cout << pi << endl;
+
+    long long i = 123456789123456789;
+    cout << i;
+
+    char c = 'c';
+    cout << c << endl;
+
 	return 0;
 }
