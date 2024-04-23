296727433f19d05ef020d0111242c26772397c56
crudapi <admin@crudapi.cn>
2024-04-23 19:02:35 +0800

    derived

---
+++ src/CMakeDerivedProject/CMakeDerivedProject.cpp
@@ -0,0 +1,39 @@
+﻿// CMakeDerivedProject.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeDerivedProject.h"
+
+using namespace std;
+
+class Base //基类
+{
+public:
+	void fun(void) { cout << "Base fun()" << endl; }
+public:
+	int a;
+};
+
+//派生类Derived
+//Derived类继承于基类Base
+class Derived : public Base
+{
+public:
+	void g(void) { cout << "Derived g()" << endl; }
+public:
+	int b;
+};
+
+int main()
+{
+	Derived d;
+	//使用基类Base的成员
+	d.a = 1;
+	d.fun();
+
+	cout << d.a << endl;
+
+	//使用派生类Derived的成员
+	d.b = 2;
+	d.g();
+	cout << d.b << endl;
+}
