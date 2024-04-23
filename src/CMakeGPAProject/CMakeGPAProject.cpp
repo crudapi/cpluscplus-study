b5aeecabc02eb296a3eade1bb03ad259ca7f8f09
crudapi <admin@crudapi.cn>
2024-04-23 16:15:58 +0800

    BubbleSort

--- src/CMakeGPAProject/CMakeGPAProject.cpp
+++ src/CMakeGPAProject/CMakeGPAProject.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void BubbleSort(int* arr, int n)
+void BubbleSort1(int* arr, int n)
 {
     for (int i = 0; i < n; ++i) {
         for (int j = 1; j < n - i; j++) {
@@ -28,7 +28,7 @@ void BubbleSort(int* arr, int n)
     }
 }
 
-int Sort()
+int Sort1()
 {
     int n = 8;
     int* arr = new int[n] {49, 38, 65, 97, 76, 13, 27, 49};//申请8个int变量,并初始化
@@ -38,6 +38,59 @@ int Sort()
         cout << arr[i] << " ";
     }
     cout << endl;
+    //执行排序
+    BubbleSort1(arr, 8);
+
+    //输出排序后的序列
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    delete[] arr;//释放动态数组需要用delete[]
+    return 0;
+}
+
+
+//打印数组中的每一个元素
+void print_array(int* arr, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+//swap two number
+void Swap(int& a, int& b)
+{
+    int tmp = a; a = b; b = tmp;
+}
+
+//put max element to the end
+void Bubble(int* arr, int length)
+{
+    for (int j = 1; j < length; j++) {
+        if (arr[j - 1] > arr[j]) { // swap if out of order [j-1] and [j]
+            //(2) your code
+            Swap(arr[j - 1], arr[j]);
+        }
+    }
+}
+
+void BubbleSort(int* arr, int n)
+{
+    for (int i = 0; i < n; i++) { // i-th pass
+        Bubble(arr, n - i);//put max element to the end
+        print_array(arr, n);
+    }
+}
+
+int BubbleSortDemo()
+{
+    int n = 8;
+    int* arr = new int[n] {49, 38, 65, 97, 76, 13, 27, 49};//申请8个int变量,并初始化
+
     //执行排序
     BubbleSort(arr, 8);
 
@@ -92,7 +145,11 @@ int main()
     delete[] score_list;
     delete[] credit_hour_list;
 
-    Sort();
+    Sort1();
+
+
+    cout << endl;
+    BubbleSortDemo();
 
     return 0;
 }
