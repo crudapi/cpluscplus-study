df62dac2773a57f6c824126587ed760f7ce45e94
crudapi <admin@crudapi.cn>
2024-04-23 16:22:37 +0800

    Student

---
+++ src/CMakeClassProject/CMakeClassProject.cpp
@@ -0,0 +1,28 @@
+﻿// CMakeClassProject.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeClassProject.h"
+
+using namespace std;
+
+struct Student
+{
+    long long id;//学号
+    string name;// 姓名
+    int age;//年龄
+};
+
+int main(void)
+{
+    Student student;
+    student.id = 2022092345;
+    student.name = "Newton";
+    student.age = 20;
+
+    cout << "id = " << student.id << endl;
+    cout << "name = " << student.name << endl;
+    cout << "age = " << student.age << endl;
+
+
+    return 0;//main end
+}
