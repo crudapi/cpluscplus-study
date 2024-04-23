658edd60ac89a5c43cdea7ff84d78b5157cc16dd
crudapi <admin@crudapi.cn>
2024-04-23 19:55:08 +0800

    stringDemo();

--- src/CMakeDerivedProject/CMakeDerivedProject.cpp
+++ src/CMakeDerivedProject/CMakeDerivedProject.cpp
@@ -44,6 +44,65 @@ int vectorDemo()
 	return 0;
 };
 
+int maxDemo()
+{
+	vector<int> arr{ 3,1,4,1,5,9,2,6 };
+
+	int max = arr[0];
+	for (int i = 1; i < arr.size(); ++i)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+
+		}
+	}
+
+	cout << "max=" << max;
+
+	return 0;
+}
+
+int max2(int a, int b)
+{
+	return a >= b ? a : b;
+}
+
+int max2Demo()
+{
+	vector<int> arr{ 3,1,4,1,5,9,2,6 };
+
+	int result_max = arr[0];
+
+	for (int i = 1; i < arr.size(); ++i)
+	{
+		result_max = max2(result_max, arr[i]);
+	}
+
+	cout << "max=" << result_max;
+
+	return 0;
+}
+
+int stringDemo()
+{
+	string you_say("I love you");
+	cout << "字符串内容:" << you_say << endl;//字符串可以直接输出
+	cout << "字符串长度:" << you_say.size() << endl;
+	cout << "字符串内容:";
+	for (size_t i = 0; i < you_say.size(); ++i)
+	{
+		cout << you_say[i];//使用下标遍历字符串
+	}
+	cout << endl;
+	//拼接字符串
+	string i_say = you_say;//复制构造一个新字符串
+	i_say += " too";//字符串拼接
+	cout << "字符串内容:" << i_say << endl;//字符串可以直接输出
+
+	return 0;
+}
+
 int main()
 {
 	Derived d;
@@ -59,4 +118,10 @@ int main()
 	cout << d.b << endl;
 
 	vectorDemo();
+
+	maxDemo();
+
+	max2Demo();
+
+	stringDemo();
 }
