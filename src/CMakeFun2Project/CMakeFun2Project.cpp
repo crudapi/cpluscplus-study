cdd886b91570a5f77591931cd09ba0e39f100430
crudapi <admin@crudapi.cn>
2024-04-23 10:33:18 +0800

    CommFactor

--- src/CMakeFun2Project/CMakeFun2Project.cpp
+++ src/CMakeFun2Project/CMakeFun2Project.cpp
@@ -21,6 +21,24 @@ void print(int n)
     cout << endl;
 }
 
+int CommFactor(int m, int n)
+{
+    cout << m << " 和 " << n << " 的最大公约数 ：";
+
+    int z = m % n;
+    while (z != 0)//不管m和n谁大谁小，都不影响求余，参考上文图片
+    {
+        m = n;
+        //(1) your code 
+        n = z;
+
+        z = m% n;
+    }
+    //n总是最后一次不会0的值，作为返回值
+    cout << n << endl;
+    return n;
+}
+
 int main()
 {
     print(123456);
@@ -28,5 +46,15 @@ int main()
     print(143);
     print(345);
     print(1);
+
+    CommFactor(1, 1);
+    CommFactor(2, 4);
+    CommFactor(4, 2);
+    CommFactor(2, 8);
+    CommFactor(8, 2);
+    CommFactor(10000, 10);
+    CommFactor(7, 9);
+    CommFactor(1997, 615);
+
 
 }
