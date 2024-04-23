79e9c7c6328fcb572ef004328a5313d2e8f18326
crudapi <admin@crudapi.cn>
2024-04-23 10:53:40 +0800

    prime number

--- src/CMakeFun2Project/CMakeFun2Project.cpp
+++ src/CMakeFun2Project/CMakeFun2Project.cpp
@@ -54,6 +54,38 @@ void print2(unsigned int n)
 
 }
 
+bool is_prime_number(int n)
+{
+    //最小的素数为 2
+    if (n < 2)
+    {
+        return false;//不是素数
+    }
+
+    for (int i = 2; i < n; ++i)
+    {
+        if (n % i == 0) //不是素数
+        {
+            //(1) your code  
+            return false;
+        }
+    }
+    //前面都没有返回false的情况下，才会走到这里，此时已经确定该数字为素数
+    return true;
+}
+
+void test(int n)
+{
+    bool is = is_prime_number(n);
+    if (is)
+    {
+        cout << n << " is prime number." << endl;
+    }
+    else
+    {
+        cout << n << " is not prime number." << endl;
+    }
+}
 
 int main()
 {
@@ -78,4 +110,11 @@ int main()
     print2(300);
     cout << endl << "print2" << endl;
     print2(1);
+
+    test(3);
+    test(4);
+    test(5);
+    test(99);
+    test(100);
+    test(101);
 }
