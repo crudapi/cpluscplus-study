9e4fac56e073d6fb46a706b4722e616d32672eb0
crudapi <admin@crudapi.cn>
2024-04-23 11:33:46 +0800

    nullptr

--- src/CMakePointProject/CMakePointProject.cpp
+++ src/CMakePointProject/CMakePointProject.cpp
@@ -8,9 +8,26 @@ using namespace std;
 
 void g(void) {     //3
 	cout << "g" << endl;
-	int a;
+	int* a = new int;
 	int b;
+	*a = 1;
+	b = 2;
+
+	cout << *a << endl;
+	cout << a << endl;
+
+	delete a;//释放a指向的无名变量
 	
+	cout << *a << endl;
+	cout << a << endl;
+
+	a = nullptr;//地址变量初始化
+
+	if (a == nullptr)//地址变量判断相等
+	{
+		cout << "pointed to nothing!" << endl;
+	}
+
 }                 //4
 void f(void) {     //2
 	cout << "f" << endl;
@@ -18,6 +35,27 @@ void f(void) {     //2
 	int y;
 	g();
 }                 //5
+
+int nullptrDemo(void)
+{
+	int* ptr = nullptr;//地址变量初始化
+
+	if (ptr == nullptr)//地址变量判断相等
+	{
+		cout << "pointed to nothing!" << endl;
+	}
+
+	//开辟一个堆变量交给 ptr 管理
+	ptr = new int(5);
+
+	if (ptr != nullptr)//地址变量判断不相等
+	{
+		cout << "pointed to " << *ptr << endl;
+	}
+	delete ptr;
+
+	return 0;
+}
 
 int main()
 {
@@ -35,6 +73,8 @@ int main()
 	cout << p << endl;
 
 	f();
+
+	nullptrDemo();
 
 	return 0;
 }
