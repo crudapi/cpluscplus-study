d689718df1fbd8f607d7fdfc330387f7440638cb
crudapi <admin@crudapi.cn>
2024-04-24 11:00:28 +0800

    current_id

---
+++ src/CMakeCurrentIdProject/CMakeCurrentIdProject.cpp
@@ -0,0 +1,31 @@
+﻿// CMakeCurrentIdProject.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeCurrentIdProject.h"
+
+using namespace std;
+
+int current_id = 0;
+
+struct Student
+{
+	Student(void) :id(current_id)
+	{
+		cout << " Student() id=" << id << endl;
+		++current_id;
+	}
+	~Student()
+	{
+		cout << "~Student() id=" << id << endl;
+	}
+	int id = 0;
+};
+
+int main(void)
+{
+	Student s1;
+	Student s2;
+	Student s3;
+
+	return 0;
+}
