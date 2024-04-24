6709aee3735caf7ff5bebfa29133cb76b4203072
crudapi <admin@crudapi.cn>
2024-04-24 10:46:10 +0800

    removeDemo()

--- src/CMakeSortProject/CMakeSortProject.cpp
+++ src/CMakeSortProject/CMakeSortProject.cpp
@@ -32,6 +32,74 @@ int sortlambda(void)
 
     return 0;
 }
+
+bool myfunction(int i, int j) { return (i < j); }
+
+struct myclass {
+    bool operator() (int i, int j) { 
+        cout << i << "<" << j << "?" << endl;
+        return (i < j); 
+    }
+} myobject;
+
+int myfunctionDemo() {
+    int myints[] = { 32,71,12,45,26,80,53,33 };
+    std::vector<int> myvector(myints, myints + 8);               // 32 71 12 45 26 80 53 33
+
+    // using default comparison (operator <):
+    std::sort(myvector.begin(), myvector.begin() + 4);           //(12 32 45 71)26 80 53 33
+
+    // using function as comp
+    std::sort(myvector.begin() + 4, myvector.end(), myfunction); // 12 32 45 71(26 33 53 80)
+
+    // using object as comp
+    std::sort(myvector.begin(), myvector.end(), myobject);     //(12 26 32 33 45 53 71 80)
+
+    // print out content:
+    std::cout << "myvector contains:";
+    for (std::vector<int>::iterator it = myvector.begin(); it != myvector.end(); ++it)
+        std::cout << ' ' << *it;
+    std::cout << '\n';
+
+    return 0;
+}
+
+int eraseDemo()
+{
+    vector<int> arr{ 1,2,3,4,5,3,9,3 };
+    /*find 找到第一个指定值的元素，
+      并返回这个元素对应的迭代器
+    */
+    auto itr = find(arr.begin(), arr.end(), 3);
+    arr.erase(itr);// 删除第1个3
+
+    for (auto item : arr)
+    {
+        cout << item << " ";
+    }
+    return 0;
+}
+
+int removeDemo()
+{
+    vector<int> arr{ 1,3,2,3,4,5,3,9,3 };
+    /*remove 把需要保留的数据紧凑的保留在数组的前面，
+     并返回第一个不应该再属于数组的元素的迭代器，
+     供后续按照范围删除
+     remove 之后数组元素排列如下：
+     1, 2, 4, 5, 9, #, #, #
+    */
+    auto itr = remove(arr.begin(), arr.end(), 3);
+    arr.erase(itr, arr.end());// 删除 { # # # }
+
+    //now print all int in arr : no 3 anymore.
+    for (auto item : arr)
+    {
+        cout << item << " ";
+    }
+    return 0;
+}
+
 int main(void)
 {
     std::vector<int> numberArray{ 1,3,5,7,9,2,4,6,8 };
@@ -45,5 +113,10 @@ int main(void)
 
     stringSort();
     sortlambda();
+    myfunctionDemo();
+
+    eraseDemo();
+    removeDemo();
+
     return 0;
 }
