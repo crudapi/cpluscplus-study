02ae8e9030ac4e07d668672e4b12ebb16ddc1da7
crudapi <admin@crudapi.cn>
2024-04-24 09:35:50 +0800

    sortlambda

--- src/CMakeSortProject/CMakeSortProject.cpp
+++ src/CMakeSortProject/CMakeSortProject.cpp
@@ -3,10 +3,35 @@
 
 #include "CMakeSortProject.h"
 #include <vector>
+#include <string>
 #include <algorithm>
 
 using namespace std;
 
+int stringSort(void)
+{
+    std::vector<std::string> nameArray{ "zhang_san","li_si"
+                 ,"wang_er","ma_zi","cao_cao", "liu_bei" };
+    std::sort(nameArray.begin(), nameArray.end());
+    for (auto name : nameArray)//范围for循环，name会遍历nameArray中的每一个元素
+    {
+        std::cout << name << std::endl;//输出nameArray中的每一个元素
+    }
+
+    return 0;
+}
+int sortlambda(void)
+{
+    std::vector<int> numberArray{ 1,3,5,7,9,2,4,6,8 };
+    /* sort 接受一个函数对象（比如，lambda表达式）*/
+    std::sort(numberArray.begin(), numberArray.end(), [](int a, int b) {return a > b; });
+    for (auto i : numberArray)//范围for循环，i会遍历每一个numberArray中的元素
+    {
+        std::cout << i << " ";//输出numberArray中的每一个元素
+    }
+
+    return 0;
+}
 int main(void)
 {
     std::vector<int> numberArray{ 1,3,5,7,9,2,4,6,8 };
@@ -18,5 +43,7 @@ int main(void)
         std::cout << i << " ";//输出numberArray中的每一个元素
     }
 
+    stringSort();
+    sortlambda();
     return 0;
 }
