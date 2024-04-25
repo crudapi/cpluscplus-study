c996fedcdac2fc791e925af1f92777f58dd7757b
crudapi <admin@crudapi.cn>
2024-04-25 15:57:50 +0800

    friend

---
+++ src/CMakeOutProject/CMakeOutProject.cpp
@@ -0,0 +1,52 @@
+﻿// CMakeOutProject.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeOutProject.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+//学生类
+class Student
+{
+	//输出操作符重载：将Student对象的数据输出到目的地os
+	//os可以是控制台屏幕cout和文件ofstream对象，他们都可以看做是ostream对象
+	//友元friend表示这个函数不是类的成员函数，而是一个全局函数，
+	//而且可以访问类的私有成员：不然在函数内访问类的私有成员就只能写stu.get_id() stu.get_name() stu.get_age()这样的代码
+	friend std::ostream& operator<<(std::ostream& os, const Student& stu);
+public:
+	std::string m_id;//学号
+	std::string m_name;//姓名
+	int m_age;//年龄
+	std::string m_date;//生日
+};
+
+std::ostream& operator<<(std::ostream& os, const Student& stu)
+{
+	//向os输出Student对象的每一个成员变量，从而将Student输出到os
+	os
+		//如果这个函数不是friend，这里就只能写 stu.get_id()这样的代码来获取类的成员变量，比较麻烦
+		<< stu.m_id << " "
+		<< stu.m_name << " "
+		<< stu.m_age << " "
+		<< stu.m_date;
+	return os;//这样os就可以连续输出多个对象。例如， cout<<stu1<<stu2;
+}
+
+int main(int argc, char** argv)
+{
+	//定义一个学生对象
+	Student stu{ "001", "zhangsan", 18, "1995-09-08" };
+	//将学生信息再次输出到控制台
+	//这里可以直接输出stu对象
+	std::cout << "学生信息：" << stu << std::endl;
+	
+	std::ofstream fout("students.txt");
+	//输出学生信息到文件
+	fout << stu << std::endl;
+
+	return 0;
+}
---
+++ src/CMakePositionsProject/CMakePositionsProject.cpp
@@ -0,0 +1,113 @@
+﻿// CMakePositionsProject.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakePositionsProject.h"
+
+#include <vector>
+#include <sstream> //istringstream
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+/*read position data from text file positions.txt
+* in vector<string>
+*/
+vector<string> read_position()
+{
+    ifstream fin("positions.txt");//用输入文件对象fin打开文本文件position.txt用来读取数据
+    if (!fin.is_open())//如果没有找到文件，则输出错误信息并退出程序，返回一个空的二维数组
+    {
+        std::cout << "error ! can not open file positions.txt!" << std::endl;
+        return vector<string>();//调用vector的默认构造函数，返回一个空的vector
+    }
+    char c = 0;
+    int row = 0, col = 0;//用来存放文件中行数和列数
+
+    fin >> col;//先读两个字符w=跳过，再读每一行的列数 53
+    fin >> row;//先读两个字符h=跳过，再读行数。19
+    //创建一个19行，每行53个字符的二维数组，每个元素的值都是空格（打印的时候看不出打印了）
+    vector<string> matrix(row, string(col, ' '));
+
+    while (!fin.eof())
+    {
+        int x = 0, y = 0, value = 0;
+        /*convert int value of char to char
+        * for example: int 32  => char ' '
+        *              int 61  => char '='
+        *              int 124 => char '|'
+        *              int 30  => char '$'
+        */
+
+        //读取一组数据（横坐标，纵坐标，值）
+        //(1) your code
+        fin >> x;
+        fin >> y;
+        fin >> value;
+        //cout << x << y << value;
+
+        //读到一组记录，就把数据信息保存到数组对应位置
+        matrix[x][y] = static_cast<char>(value);//类型转换
+    }
+    return matrix;
+}
+
+void print_matrix(const vector<string>& matrix)
+{
+    //逐行打印
+    for (int row = 0; row < matrix.size(); ++row)
+    {
+        //string可以直接整体打印
+        //(2) your code
+        cout << matrix[row] << endl;
+    }
+}
+int istringstreamDemo()
+{
+    //字符串数据（注意字符串中间有一个空格）：用来区分是两个数据一个是年龄，一个是姓名
+    string data("23 Jay");
+
+    //创建一个istringstream对象iss，用字符串数据初始化对象iss
+    istringstream iss(data);
+
+    int age;
+    string name;
+
+    //从字符串流中读取数据赋值给变量
+    iss >> age >> name;
+
+    //输出变量
+    cout << "age=" << age << ", name=" << name << endl;
+    return 0;
+}
+
+int ostringstreamDemo()
+{
+    //创建一个ostringstream对象oss，准备存储外部数据
+    ostringstream oss;
+
+    int age = 23;
+    string name("Jay");
+
+    //将外部数据存入（写入）到字符串流中
+    oss << "age=" << age << ", name=" << name << endl;
+
+    //输出字符串流中的内容
+    cout << oss.str();
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    //读取数据到二维字符数组
+    auto matrix = read_position();
+    //打印二维数组
+    //(3) your code
+    print_matrix(matrix);
+
+    istringstreamDemo();
+
+    ostringstreamDemo();
+
+    return 0;
+}
