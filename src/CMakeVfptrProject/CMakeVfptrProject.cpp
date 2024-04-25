0c2d821e285efe7436ccdb660ba8960f89928e41
crudapi <admin@crudapi.cn>
2024-04-25 14:41:39 +0800

    size

--- src/CMakeVfptrProject/CMakeVfptrProject.cpp
+++ src/CMakeVfptrProject/CMakeVfptrProject.cpp
@@ -4,11 +4,19 @@
 #include "CMakeVfptrProject.h"
 
 using namespace std;
+class Normal {
+private:
+	int num;
+	int num2;
+	char a;
+};
 class Base {
 private:
 	int num;
+	int num2;
+	char a;
 public:
-	Base() { num = 10; }
+	Base() { num = 10; num2 = 20; a = 'Q'; }
 	~Base() {}
 	virtual void virFunc1() {				//基类虚函数1
 		cout << "Base中虚函数1" << endl;
@@ -16,19 +24,31 @@ public:
 	virtual void virFunc2() {
 		cout << "Base中虚函数2" << endl;	//基类虚函数2
 	}
-	virtual void virFunc3() {
-		cout << "Base中虚函数3" << endl;	//基类虚函数3
-	}
 };
 class Son :public Base {
+private:
+	int sonNum;
 public:
 	virtual void virFunc2() {				//派生类重写虚函数2
 		cout << "Son中虚函数2" << endl;
 	}
 };
-void test01() {
+
+void sizeDemo() {
+	Normal normal;
 	Base base;
-	intptr_t* vfptr = (intptr_t*)&base;		//在对象储存空间开始处取出一个指针变量，这个指针指向虚函数表（也就是存放虚函数地址的数组）
+	Son son;
+	cout << "sizeof(int) = " << sizeof(int) << endl;
+	cout << "sizeof(char) = " << sizeof(char) << endl;
+	cout << "sizeof(normal) = " << sizeof(normal) << endl;
+	cout << "sizeof(base) = " << sizeof(base) << endl;
+	cout << "sizeof(son) = " << sizeof(son) << endl;
+}
+
+void test01() {
+	Son son;
+	Base& base = son;
+	intptr_t* vfptr = (intptr_t*)&son;		//在对象储存空间开始处取出一个指针变量，这个指针指向虚函数表（也就是存放虚函数地址的数组）
 	intptr_t* vftable = (intptr_t*)*vfptr;	//对虚函数表的指针取值，取得虚函数表，可理解为数组名
 	void(*func)() = (void(*)()) * vftable;	//对虚函数表的数组名取值，得到第一个元素，也就是第一个虚函数的地址
 	func();
@@ -37,5 +57,7 @@ void test01() {
 }
 int main(int argc, char const** argv) {
 	test01();
+
+	sizeDemo();
 	return 0;
 }
