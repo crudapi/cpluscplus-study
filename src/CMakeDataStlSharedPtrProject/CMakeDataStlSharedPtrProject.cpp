f317f7b853f5eef3fc48cded0b7c3db250a8d14d
crudapi <admin@crudapi.cn>
2024-04-28 20:17:55 +0800

    shared_ptr

--- src/CMakeDataStlSharedPtrProject/CMakeDataStlSharedPtrProject.cpp
+++ src/CMakeDataStlSharedPtrProject/CMakeDataStlSharedPtrProject.cpp
@@ -6,13 +6,37 @@
 #include<memory>//shared_ptr 
 using namespace std;
 
+
+struct Student
+{
+    Student(void)
+    {
+        cout << "construct Student()" << endl;
+    }
+    ~Student(void)
+    {
+        cout << "destruct ~Student()" << endl;
+    }
+};
+
 int main()
 {
-    //you get a heap int stored 123 for p
-    shared_ptr<int> p(new int(123));
+    {
+        Student* s = new Student;
+    }
+    cout << "main begin" << endl;
+
+    //you get a heap Student hold by p
+    shared_ptr<Student> p(new Student);
+
+    shared_ptr<Student> p2 = p;
 
     //you do not need to delete something here,
     // and p will delete the heap int automatically
+
+    cout << "main end" << endl;
+
+    
 
     return 0;
 }
