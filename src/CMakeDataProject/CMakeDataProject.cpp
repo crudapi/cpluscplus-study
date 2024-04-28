54bcf6f0ac90363028e8b8da053e5f0495ca18be
crudapi <admin@crudapi.cn>
2024-04-28 19:06:52 +0800

    autoptr

---
+++ src/CMakeDataProject/CMakeDataProject.cpp
@@ -0,0 +1,27 @@
+﻿// CMakeDataProject.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeDataProject.h"
+
+#include <iostream>
+using namespace std;
+
+struct AutoPtr
+{
+    AutoPtr(int* source) :m_p(source)
+    {
+    }
+    ~AutoPtr()
+    {
+        delete m_p;
+    }
+    int* m_p = nullptr;
+};
+
+int main()
+{
+    AutoPtr ptr(new int(123));
+    cout << *ptr.m_p << endl;
+
+    return 0;
+}
