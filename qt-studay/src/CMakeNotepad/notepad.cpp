08bb6d814903910d2d7cc8af06e89d4d0a5afa14
crudapi <admin@crudapi.cn>
2024-04-29 19:49:12 +0800

    file

--- qt-studay/src/CMakeNotepad/notepad.cpp
+++ qt-studay/src/CMakeNotepad/notepad.cpp
@@ -31,10 +31,36 @@ NotePad::NotePad(QWidget *parent)
 
 void NotePad::load()
 {
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), "",
+        tr("Text Files (*.txt);;C++ Files (*.cpp *.h)"));
 
+    if (!fileName.isEmpty()) {
+        QFile file(fileName);
+        if (!file.open(QIODevice::ReadOnly)) {
+            QMessageBox::critical(this, tr("Error"), tr("Could not open file"));
+            return;
+        }
+        QTextStream in(&file);
+        textEdit->setText(in.readAll());
+        file.close();
+    }
 }
 
 void NotePad::save()
 {
 
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), "",
+        tr("Text Files (*.txt);;C++ Files (*.cpp *.h)"));
+
+    if (!fileName.isEmpty()) {
+        QFile file(fileName);
+        if (!file.open(QIODevice::WriteOnly)) {
+            // error message
+        } else {
+            QTextStream stream(&file);
+            stream << textEdit->toPlainText();
+            stream.flush();
+            file.close();
+        }
+    }
 }
