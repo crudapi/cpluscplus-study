a29f899bdfe90b87bd6d1c1583b81a1185491704
crudapi <admin@crudapi.cn>
2024-05-02 11:33:51 +0800

    stdJoin

---
+++ src/CMakeJoinProject/CMakeJoinProject.cpp
@@ -0,0 +1,37 @@
+﻿// CMakeJoinProject.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeJoinProject.h"
+
+#include <thread>
+#include <iostream>
+using namespace std;
+
+void thread_fun1(void)
+{
+    cout << "one STL thread 1!" << endl;
+}
+
+void thread_fun2(void)
+{
+    cout << "one STL thread 2!" << endl;
+}
+
+int stdJoin(void)
+{
+    std::thread thread1(thread_fun1);
+    std::thread thread2(thread_fun2);
+
+    thread1.join();
+    thread2.join();
+
+    return 0;
+}
+
+
+int main(void)
+{
+    stdJoin();
+
+    return 0;
+}
