b008ff06a4c6c0734196a119b89333179e7abcfe
crudapi <admin@crudapi.cn>
2024-05-06 11:26:36 +0800

    gdb

---
+++ linux/gdb/hello.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	int a = 8889;
+
+	cout << a << endl;
+
+
+	cout << "1 line" << endl;
+	cout << "2 line" << endl;
+	cout << "3 line" << endl;
+	cout << "4 line" << endl;
+	cout << "5 line" << endl;
+	cout << "6 line" << endl;
+	cout << "7 line" << endl;
+	cout << "8 line" << endl;
+	cout << "9 line" << endl;
+	cout << "10 line" << endl;
+	cout << "11 line" << endl;
+
+	return 0;
+}
---
+++ linux/gdb/readme.txt
@@ -0,0 +1,6 @@
+g++ hello.cpp -o hello
+./hello
+
+g++ -g hello.cpp -o hello
+
+gdb hello
