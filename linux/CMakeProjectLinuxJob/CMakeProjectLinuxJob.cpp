cbeb918456c2a0e620f93e3aba4fcc1d1ffd943f
crudapi <admin@crudapi.cn>
2024-05-06 15:15:42 +0800

    job

---
+++ linux/CMakeProjectLinuxJob/CMakeProjectLinuxJob.cpp
@@ -0,0 +1,15 @@
+﻿// CMakeProjectLinuxJob.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeProjectLinuxJob.h"
+
+#include <unistd.h>
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << "hello,world" << endl;
+	sleep(10000);
+	cout << "byebye" << endl;
+}
