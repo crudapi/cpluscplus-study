a778f3e513f88437c35044bc1fa66b63aebb98b8
crudapi <admin@crudapi.cn>
2024-05-06 19:36:53 +0800

    pthread_create

--- linux/CMakeProjectPthread/CMakeProjectPthread.cpp
+++ linux/CMakeProjectPthread/CMakeProjectPthread.cpp
@@ -9,22 +9,24 @@
 
 void* thfunc(void* arg)
 {
-	printf("in thfunc\n");
+	int* pn = (int*)(arg);
+	printf("in thfunc %d\n", *pn);
 	return (void*)0;
 }
 int main(int argc, char* argv[])
 {
 	pthread_t tidp;
-	int ret;
+	int ret, n = 110;
 
-	ret = pthread_create(&tidp, NULL, thfunc, NULL);
+
+	ret = pthread_create(&tidp, NULL, thfunc, &n);
 	if (ret)
 	{
 		printf("pthread_create failed:%d\n", ret);
 		return -1;
 	}
 
-	sleep(1);
+	pthread_join(tidp, NULL);
 	printf("in main:thread is created\n");
 
 	return 0;
