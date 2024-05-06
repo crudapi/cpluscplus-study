3c51b11b42a54b3edb9d574e6e0964bef7e5ab95
crudapi <admin@crudapi.cn>
2024-05-06 13:55:45 +0800

    mmap

--- linux/CMakeProjectLinuxFile/CMakeProjectLinuxFile.cpp
+++ linux/CMakeProjectLinuxFile/CMakeProjectLinuxFile.cpp
@@ -6,8 +6,10 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/mman.h> /* for mmap and munmap */  
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h> /* for memcpy */  
 
 using namespace std;
 
@@ -27,6 +29,37 @@ int openFile()
 
 	return fd;
 }
+
+int mmapDemo()
+{
+	int fd;
+	char* mapped_mem, * p;
+	int flength = 1024;
+	void* start_addr = 0;
+
+	fd = open("/home/doubleming/temp/test2.txt", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	flength = lseek(fd, 1, SEEK_END);
+	write(fd, "\0", 1);
+	lseek(fd, 0, SEEK_SET);
+	start_addr = (void*)0x80000;
+	mapped_mem = (char*)mmap(start_addr,
+		flength,
+		PROT_READ | PROT_WRITE, 
+		MAP_SHARED,
+		fd,
+		0);
+
+	printf("%s\n", mapped_mem);
+	while ((p = strstr(mapped_mem, "hello"))) { 
+		memcpy(p, "Linux", 5);
+		p += 5;
+	}
+
+	close(fd);
+	munmap(mapped_mem, flength);
+	return 0;
+}
+
 
 int main()
 {
@@ -50,5 +83,10 @@ int main()
 
 	openFile();
 
+	mmapDemo();
+
 	return 0;
 }
+
+
+
