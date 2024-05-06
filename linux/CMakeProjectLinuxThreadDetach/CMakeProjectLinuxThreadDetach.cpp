41d89b4dffad0f001b8f90c50812e9f74b933615
crudapi <admin@crudapi.cn>
2024-05-06 20:12:49 +0800

    detach

---
+++ linux/CMakeProjectLinuxThreadDetach/CMakeProjectLinuxThreadDetach.cpp
@@ -0,0 +1,60 @@
+﻿// CMakeProjectLinuxThreadDetach.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeProjectLinuxThreadDetach.h"
+
+#ifndef _GNU_SOURCE  
+#define _GNU_SOURCE     /* To get pthread_getattr_np() declaration */  
+#endif  
+#include <pthread.h>  
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <unistd.h>  
+#include <errno.h>  
+
+static void* thread_start(void* arg)
+{
+	int i, s;
+	pthread_attr_t gattr;
+
+	s = pthread_getattr_np(pthread_self(), &gattr);
+	if (s != 0)
+		printf("pthread_getattr_np failed\n");
+
+	s = pthread_attr_getdetachstate(&gattr, &i);
+	if (s)
+		printf("pthread_attr_getdetachstate failed");
+	printf("Detach state        = %s\n",
+		(i == PTHREAD_CREATE_DETACHED) ? "PTHREAD_CREATE_DETACHED" :
+		(i == PTHREAD_CREATE_JOINABLE) ? "PTHREAD_CREATE_JOINABLE" :
+		"???");
+
+	pthread_detach(pthread_self()); //×ª»»Ïß³ÌÎª¿É·ÖÀëÏß³Ì
+
+	s = pthread_getattr_np(pthread_self(), &gattr);
+	if (s != 0)
+		printf("pthread_getattr_np failed\n");
+	s = pthread_attr_getdetachstate(&gattr, &i);
+	if (s)
+		printf(" pthread_attr_getdetachstate failed");
+	printf("after pthread_detach,\nDetach state        = %s\n",
+		(i == PTHREAD_CREATE_DETACHED) ? "PTHREAD_CREATE_DETACHED" :
+		(i == PTHREAD_CREATE_JOINABLE) ? "PTHREAD_CREATE_JOINABLE" :
+		"???");
+
+	pthread_attr_destroy(&gattr);  //Ïú»ÙÊôÐÔ
+}
+
+int main(int argc, char* argv[])
+{
+	pthread_t thread_id;
+	int s;
+
+	s = pthread_create(&thread_id, NULL, &thread_start, NULL);
+	if (s != 0)
+	{
+		printf("pthread_create failed\n");
+		return 0;
+	}
+	pthread_exit(NULL);//Ö÷Ïß³ÌÍË³ö£¬µ«½ø³Ì²¢²»ÂíÉÏ½áÊø
+}
