e8d07e9cf0b9a3892afe9fb8b8a17a648e93426e
crudapi <admin@crudapi.cn>
2024-05-06 10:40:28 +0800

    linux

---
+++ linux/LinuxConsoleApplication/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    printf("%s 向你问好!\n", "LinuxConsoleApplication");
+    int a = 8;
+    cout << a << endl;
+    return 0;
+}
