da9a72bff47b558ea98f4a16ab3ba7010c4ecd62
crudapi <admin@crudapi.cn>
2024-05-06 19:44:09 +0800

    MYSTRUCT

--- linux/CMakeProjectPthread/CMakeProjectPthread.cpp
+++ linux/CMakeProjectPthread/CMakeProjectPthread.cpp
@@ -13,6 +13,41 @@ void* thfunc(void* arg)
 	printf("in thfunc %d\n", *pn);
 	return (void*)0;
 }
+
+typedef struct  //定义结构体的类型
+{
+	int n;
+	const char* str;
+}MYSTRUCT;
+
+void* thfunc2(void* arg)
+{
+	MYSTRUCT* p = (MYSTRUCT*)arg;
+	printf("in thfunc:n=%d,str=%s\n", p->n, p->str); //打印结构体的内容
+	return (void*)0;
+}
+int mainDemo()
+{
+	pthread_t tidp;
+	int ret;
+	MYSTRUCT mystruct; //定义结构体
+	//初始化结构体
+	mystruct.n = 220;
+	mystruct.str = "hello world";
+
+	ret = pthread_create(&tidp, NULL, thfunc2, (void*)&mystruct);//创建线程并传递结构体地址 
+	if (ret)
+	{
+		printf("pthread_create failed:%d\n", ret);
+		return -1;
+	}
+	pthread_join(tidp, NULL); //等待子线程结束
+	printf("in main:thread is created\n");
+
+	return 0;
+}
+
+
 int main(int argc, char* argv[])
 {
 	pthread_t tidp;
@@ -29,5 +64,9 @@ int main(int argc, char* argv[])
 	pthread_join(tidp, NULL);
 	printf("in main:thread is created\n");
 
+	mainDemo();
+
 	return 0;
 }
+
+
