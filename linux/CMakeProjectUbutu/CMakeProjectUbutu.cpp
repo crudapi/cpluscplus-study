e66c449bbef43171cbe46dbb035a7498d1c741d0
crudapi <admin@crudapi.cn>
2024-05-06 10:54:50 +0800

    linux

---
+++ linux/CMakeProjectUbutu/CMakeProjectUbutu.cpp
@@ -0,0 +1,16 @@
+﻿// CMakeProjectUbutu.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeProjectUbutu.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	int a = 888;
+
+	cout << a << endl;
+
+	return 0;
+}
