a143c8c8e6ed52137465d7e37878c037502e0367
crudapi <admin@crudapi.cn>
2024-05-07 14:09:42 +0800

    pool

--- linux/CMakeProjectLinuxThreadPool/CMakeProjectLinuxThreadPool.cpp
+++ linux/CMakeProjectLinuxThreadPool/CMakeProjectLinuxThreadPool.cpp
@@ -2,11 +2,67 @@
 //
 
 #include "CMakeProjectLinuxThreadPool.h"
-
+#include "thread_pool.h"
 using namespace std;
 
-int main()
+#include <iostream> 
+#include <thread>
+#include <mutex> 
+#include <unistd.h>
+#include <stdio.h>
+
+
+class CMyTask : public CTask
 {
-	cout << "Hello CMake." << endl;
+public:
+	CMyTask() = default;
+	int Run() 
+	{
+		printf("%s\n",  (char*)m_ptrData);
+		int x = rand() % 4 + 1;
+		sleep(x);
+		return 0;
+	}
+	~CMyTask() {};
+};
+
+int main(int argc, const char* argv[])
+{
+	CMyTask taskObj;
+
+	char strName[] = "hello";
+
+	taskObj.setData((void*)strName);
+
+	CThreadPool threadPool(5);
+
+	sleep(10);
+
+	for (int i = 0; i < 10; ++i) 
+	{
+		sleep(2);
+		threadPool.AddTask(&taskObj);
+	}
+
+
+	while (1) 
+	{
+		printf("\n %d task need handele\n", threadPool.getTaskSize());
+
+
+		if (threadPool.getTaskSize() == 0)
+		{
+			if (threadPool.StopAll() == -1)
+			{
+				printf("clear exit!\n");
+				exit(0);
+			}
+		}
+
+		sleep(2);
+
+		printf("sleep 2s\n");
+	}
+
 	return 0;
 }
